eaa0cb3c0093998ea7b5532e170f9087e539580e
Bui Thanh Dat <datbtth2011018@fpt.edu.vn>
2021-10-02 20:38:49 +0700

    ss

---
+++ Ex2/BaseClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tiger = new Tiger();
+            var lion = new Lion();
+            
+            tiger.SetMe("Tiger",100);
+            lion.SetMe("Lion",200);
+            
+            tiger.Show();
+            lion.Show();
+        }
+    }
+
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}, Weight: {Weight}");
+        }
+        public abstract void SetMe(string name, double weight);
+    }
+
+    public class Tiger : Animal
+    {
+        public override void SetMe(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public override void SetMe(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+}
---
+++ GeometryExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ex1;
+
+
+namespace GeometryExample
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the dimenstions of the cylinder");
+            Cylinder cylinder = new Cylinder();
+            cylinder.Radius = 38.64;
+            cylinder.Height = 22.48;
+            cylinder.Process();
+            cylinder.Result();
+        }
+    }
+}
